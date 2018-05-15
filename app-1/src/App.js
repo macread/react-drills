6e14968f99b2db983064aba02961a7ac9ae3f9d4
Mac Read <james.macgregor.read@gmail.com>
2018-05-14 18:02:34 -0600

    Solved app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+      
+    this.state = {
+        message: ''
+      }
+    this.updateMessage = this.updateMessage.bind(this)
+  }
+
+  updateMessage(e){
+    this.setState({message: e.target.value})
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <input type='' className='' onChange={this.updateMessage}/>
+          <p>{this.state.message}</p>
       </div>
     );
   }
